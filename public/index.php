38b7d59ea43f85f1b073cb8b4c05b4475ea0ab70
Ka Ka <caoka5265@gmail.com>
2022-01-24 13:22:07 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,13 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css">
     <title>Document</title>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
-    <h1>QUẢN LÍ FORM</h1>
-    <div class="main_nav">
-        <a href="">Nhập form</a>
-        <a href="">Quản lí Data</a>
-        <a href="">Nâng cao</a>
-    </div>
+    <div class="container">
+        <h1>QUẢN LÍ FORM</h1>
+        <div class="main_nav">
+            <a href=""><ion-icon name="add-circle"></ion-icon>Nhập form</a>
+            <a href=""><ion-icon name="layers"></ion-icon>Quản lí</a>
+            <a href=""><ion-icon name="code-slash"></ion-icon>  Nâng cao</a>
+        </div>
+    </div>    
 </body>
 </html>
