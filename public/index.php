81848fb06d6a1c21f940ccd706f9888b37f08bc5
Ka Ka <caoka5265@gmail.com>
2022-01-25 09:05:36 +0700

    Update Route

--- public/index.php
+++ public/index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/style.css">
-    <script src="JS/switchtheme.js"></script>
-    <title>Document</title>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-</head>
-<body>
-    <div class="container">
-        <h1>QUẢN LÍ FORM</h1>
-        <div class="main_nav">
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSea0RzjSzQj6oPkamF3dhZA6k0KNjREqv9uuZcIFdXsKmVaNg/viewform?usp=sf_link"><ion-icon name="add-circle"></ion-icon>Nhập form</a>
-            <a href="https://docs.google.com/spreadsheets/d/11EgqzKttF-UgqRhnOCmSG8_j2r0HfjVE5KilSnBhAG4/edit?resourcekey#gid=1688646093"><ion-icon name="document-text"></ion-icon>Excel Data</a>
-            <a href=""><ion-icon name="layers"></ion-icon>Quản lí</a>
-            <a href=""><ion-icon name="code-slash"></ion-icon>Nâng cao</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/style.css">
+        <script src="JS/switchtheme.js"></script>
+        <title>Document</title>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>QUẢN LÍ FORM</h1>
+            <div class="main_nav">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSea0RzjSzQj6oPkamF3dhZA6k0KNjREqv9uuZcIFdXsKmVaNg/viewform?usp=sf_link"><ion-icon name="add-circle"></ion-icon>Nhập form</a>
+                <a href="https://docs.google.com/spreadsheets/d/11EgqzKttF-UgqRhnOCmSG8_j2r0HfjVE5KilSnBhAG4/edit?resourcekey#gid=1688646093"><ion-icon name="document-text"></ion-icon>Excel Data</a>
+                <a href=""><ion-icon name="layers"></ion-icon>Quản lí</a>
+                <a href=""><ion-icon name="code-slash"></ion-icon>Nâng cao</a>
+            </div>
         </div>
-    </div>
-    <div id="switchthemme_btn" onclick="switchTheme();"><ion-icon name="sparkles"></ion-icon></div>    
-</body>
+        <div id="switchthemme_btn" onclick="switchTheme();"><ion-icon name="sparkles"></ion-icon></div>    
+    </body>
 </html>
