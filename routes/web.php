4f2d78e3eb84f1650c34b1e8b2f8a63366b1e96a
Ka Ka <caoka5265@gmail.com>
2022-02-13 15:00:26 +0700

    commit on kaka

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/welcome1', function () {
+    return view('welcome');
+});
