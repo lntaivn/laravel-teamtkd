a7746a1cda86a1f0fc9f005b7873ea5e2a80ad98
Ka Ka <caoka5265@gmail.com>
2022-02-13 16:46:16 +0700

    commit on kaka

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/welcome1', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return view('hello');
 });
